838032adff4c147f12fe9c5ae0133db020020a3e
fernando sardenberg <leo.f.sardenberg@gmail.com>
2022-01-14 19:48:58 -0300

    moviecard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,35 @@
-function Listing(){
-   return(
-   <h1>listing stuff</h1>
-   ); 
+import Moviecard from "Components/MovieCard";
+import Pagination from "Components/pagination";
+
+function Listing() {
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <Moviecard />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <Moviecard />
+          </div>
+        </div><div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <Moviecard />
+          </div>
+        </div><div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <Moviecard />
+          </div>
+        </div><div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <Moviecard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Listing;
